9539d8ffe48130f108aba31fecebe6dab2eeeeff
Jose David Muñoz Cruz <jhosguamuc@gmail.com>
2022-11-22 12:07:32 -0600

    Components

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component(
     {
      selector:'app-server',
-     template:'./server.component.html'   
+     templateUrl:'./server.component.html'   
     }
 )
 
