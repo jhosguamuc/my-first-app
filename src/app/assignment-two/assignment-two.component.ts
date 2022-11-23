4f4bffc5354476f05ab955d00d3b43a9608eccdd
Jose David Muñoz Cruz <jhosguamuc@gmail.com>
2022-11-22 19:24:49 -0600

    assignment two completed

---
+++ src/app/assignment-two/assignment-two.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assignment-two',
+  templateUrl: './assignment-two.component.html',
+  styleUrls: ['./assignment-two.component.css']
+})
+export class AssignmentTwoComponent {
+
+username: string = "";
+
+clearInput() : void {
+  this.username = "";
+}
+
+}
